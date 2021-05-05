56914dfb27349676711cf3b183cfcb859e305fe6
harshpsyche <abhayagarwala011@gmail.com>
2021-05-05 19:22:21 +0530

    First

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,48 +1,159 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div style="display:flex;place-content:center;">
+      <input
+        type="text"
+        id="searchbar"
+        class="form-control"
+        v-model="search"
+        @input="setsearch"
+        placeholder="Search"
+      />
+    </div>
+
+    <div id="wholeFile">
+      <a href="http://localhost:8000/downloadW/">
+        <button class="btn btn-primary">Whole Day (CSV)</button>
+      </a>
+      <a :href="`http://localhost:8000/downloadC?search=${search}`">
+        <button class="btn btn-secondary">Current Result (CSV)</button>
+      </a>
+      <div v-if="!noresult">{{items.length}} items found.</div>
+    </div>
+    <div style="place-content:center;display:flex;">
+      <div
+        v-if="!noresult"
+        style="display:flex;place-content:center;place-self:center;flex-direction:column;"
+      >
+        <ul style="margin-bottom:4px">
+          <div
+            style="display:flex;font-size:20px;margin:10px 0px 10px 0pxfont-weight:bold;background:#120E43;color:white;place-content:center;"
+            class="list-group-item list-group-item-action"
+          >
+            <div id="sharename" style="color:orange">Name</div>
+            <div id="sharename" style="color:orange">Code</div>
+            <div id="sharename" style="color:lightblue">Open (₹)</div>
+            <div id="sharename" style="color:lightblue">Close (₹)</div>
+            <div id="sharename" style="color:lightgreen">High (₹)</div>
+            <div id="sharename" style="color:red">Low (₹)</div>
+          </div>
+        </ul>
+        <div
+          style="overflow:scroll;overflow-x:hidden;height:50vh;border-bottom-left-radius:10px;border-bottom-right-radius:10px;"
+        >
+          <ul>
+            <li
+              v-for="item in items"
+              v-bind:key="item.name"
+              style="display:flex;flex-direction: column;align-content:center;justify-content:center;padding-bottom:10px;place-self:center;margin:10px 10px 2px 5px"
+              class="list-group-item list-group-item-action"
+            >
+              <div style="display:flex;place-content:center;">
+                <div id="sharename" style="font-weight: bolder !important; ">{{ item.name }}</div>
+                <div id="sharename">{{ item.code }}</div>
+                <div id="sharename">{{ item.open }}</div>
+                <div id="sharename">{{ item.close }}</div>
+                <div id="sharename">{{ item.high }}</div>
+                <div id="sharename">{{ item.low }}</div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div v-if="noresult">No Results Found</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  data() {
+    return {
+      noresult: true,
+      search: "",
+      items: []
+    };
+  },
+  methods: {
+    created() {},
+    toggleNoResults() {
+      this.noresult = !this.noresult;
+    },
+    setsearch: function(event) {
+      this.search = event.target.value;
+      if (this.search === "") {
+        this.noresult = true;
+        return;
+      }
+      fetch("http://127.0.0.1:8000/search/?search=" + this.search)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data.result);
+          this.items = data.result;
+          this.noresult = false;
+        });
+    }
+  },
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+::-webkit-scrollbar {
+  width: 6px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: white;
+  border-radius: 4px;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: gray;
+  border-radius: 4px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #5daffa;
+}
+.btn {
+  margin: 10px !important;
+}
+#sharename {
+  text-align: center;
+  width: 10vw;
+  margin: 5px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+#date {
+  padding: 7px;
+  margin: 4px;
+}
+
+#wholefile {
+  display: flex;
+}
+
+#searchbar :hover {
+  background: #42b983;
+}
+#searchbar {
+  width: 40vw;
+  padding: 10px;
+}
 h3 {
-  margin: 40px 0 0;
+  margin: 0 0 0;
 }
 ul {
   list-style-type: none;
